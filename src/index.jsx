4248a7859f0bbe8931b99a08e5b9b19392876628
Jesse Beard <jjbeard@protonmail.com>
2020-05-05 02:38:34 -0700

    added core-js & regenerator-runtime refactored from promise to async/await

--- src/index.jsx
+++ src/index.jsx
@@ -1,18 +1,27 @@
 import React, { useState, useEffect } from 'react';
-
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
 import ReactDOM from 'react-dom';
 
 
 function Description() {
   const [listing, setListing] = useState();
   const [date, setDate] = useState(new Date());
+  // eslint-disable-next-line prefer-const
+  const fetchListing = async () => {
+    let response = await fetch('/api/description/1');
+    response = await response.json();
+    setListing(JSON.stringify(response));
+  };
+  // eslint-disable-next-line no-undef
 
   useEffect(() => {
-    fetch('/api/description/1')
-      .then((res) => res.json())
-      .then((res) => {
-        setListing(JSON.stringify(res));
-      });
+    // fetch('/api/description/1')
+    //   .then((res) => res.json())
+    //   .then((res) => {
+    //     setListing(JSON.stringify(res));
+    //   });
+    fetchListing();
   }, [listing]);
 
   useEffect(() => {
