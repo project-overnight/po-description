a8c1b5c76c7d91039807cd47279041526707a095
Jesse Beard <jjbeard@protonmail.com>
2020-05-16 17:04:52 -0700

    button now reflects the number of amenities.

--- src/modules/amenities/amenities.jsx
+++ src/modules/amenities/amenities.jsx
@@ -2,46 +2,51 @@ import React from 'react';
 import styles from './Amenities.css';
 import AmenitiesModal from './AmenitiesModal';
 import SortedAmenitiesList from './SortedAmenitiesList';
+import useListing from '../utilities/useListing';
 
-const Amenities = () => (
-  <>
-    <div className={styles.amenititesTitleStyling}>
-      Amenities
-    </div>
-    <div>4 things go here </div>
-    <AmenitiesModal>
-      {({ modal, openModal, closeModal }) => (
-        <>
-          <div className={styles.amenitiesModalSpacingDiv}>
-            <button
-              className={styles.amenitiesModalOpenButton}
-              type="button"
-              onClick={openModal}
-            >
-              Show all ## Amenities
-            </button>
-          </div>
-          { modal(
-            <>
-              <div className={styles.modalCloseButtonSpacingDiv}>
-                <button
-                  className={styles.modalCloseButton}
-                  type="button"
-                  tabIndex={0}
-                  onClick={closeModal}
-                >
-                  X
-                </button>
 
-              </div>
-              <span className={styles.ammenitiesModalTitle}>Ammenities</span>
-              <SortedAmenitiesList />
-            </>,
-          ) }
-        </>
-      )}
-    </AmenitiesModal>
-  </>
-);
+const Amenities = () => {
+  const { amenitiesNumber } = useListing();
+  return (
+    <>
+      <div className={styles.amenititesTitleStyling}>
+        Amenities
+      </div>
+      <div>4 things go here </div>
+      <AmenitiesModal>
+        {({ modal, openModal, closeModal }) => (
+          <>
+            <div className={styles.amenitiesModalSpacingDiv}>
+              <button
+                className={styles.amenitiesModalOpenButton}
+                type="button"
+                onClick={openModal}
+              >
+                {`Show all ${amenitiesNumber} Amenities`}
+              </button>
+            </div>
+            { modal(
+              <>
+                <div className={styles.modalCloseButtonSpacingDiv}>
+                  <button
+                    className={styles.modalCloseButton}
+                    type="button"
+                    tabIndex={0}
+                    onClick={closeModal}
+                  >
+                    X
+                  </button>
+
+                </div>
+                <span className={styles.ammenitiesModalTitle}>Ammenities</span>
+                <SortedAmenitiesList />
+              </>,
+            ) }
+          </>
+        )}
+      </AmenitiesModal>
+    </>
+  );
+};
 
 export default Amenities;
